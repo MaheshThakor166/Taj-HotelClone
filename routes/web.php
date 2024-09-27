62ba711d68150fd7abd861486b01b85297a19a5a
Mahesh Thakor <bcasemmaheshthakor@gmail.com>
2024-09-27 11:09:28 +0530

    Loggin  commited

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/Login', function () {
+    return view(view: 'Login');
+});
 
 Route::get('/Home', function () {
     return view(view: 'Home');
